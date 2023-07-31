910dbe19a43518fa70352bfaa23753303df821a0
nresare <nresare@thoughtmachine.net>
2023-07-31 12:22:31 +0100

    Move to anyhow for neater error handling, log to syslog

--- src/lib.rs
+++ src/lib.rs
@@ -9,30 +9,73 @@ use std::env;
 use pam::constants::{PamFlag, PamResultCode};
 use pam::module::{PamHandle, PamHooks};
 
+use anyhow::{anyhow, Context, Result};
 use ssh_agent_client_rs::Client;
 use std::ffi::CStr;
+use std::fmt::Display;
 use std::path::Path;
+use syslog::{Formatter3164, Logger, LoggerBackend};
 
 struct PamSshAgent;
 pam::pam_hooks!(PamSshAgent);
 
 impl PamHooks for PamSshAgent {
-    fn acct_mgmt(_pamh: &mut PamHandle, _args: Vec<&CStr>, _flags: PamFlag) -> PamResultCode {
-        PamResultCode::PAM_SUCCESS
-    }
+    fn sm_authenticate(
+        _pam_handle: &mut PamHandle,
+        _args: Vec<&CStr>,
+        _flags: PamFlag,
+    ) -> PamResultCode {
+        let mut log = SyslogLogger::new();
 
-    fn sm_authenticate(_pamh: &mut PamHandle, _args: Vec<&CStr>, _flags: PamFlag) -> PamResultCode {
-        let path = env::var("SSH_AUTH_SOCK").expect("SSH_AUTH_SOCK is not set");
-        let ssh_agent_client = Client::connect(Path::new(path.as_str())).unwrap();
-        let result = authenticate("/etc/sudo_ssh_keys", ssh_agent_client).unwrap();
-        if result {
-            PamResultCode::PAM_SUCCESS
-        } else {
-            PamResultCode::PAM_AUTH_ERR
+        match do_authenticate(&mut log) {
+            Ok(_) => PamResultCode::PAM_SUCCESS,
+            Err(err) => {
+                for line in format!("{err:?}").split('\n') {
+                    log.error(line).expect("Failed to log");
+                }
+                PamResultCode::PAM_AUTH_ERR
+            }
+        }
+    }
+}
+
+fn do_authenticate(log: &mut SyslogLogger) -> Result<()> {
+    let path = env::var("SSH_AUTH_SOCK")
+        .context("Required environment variable SSH_AUTH_SOCK is not set")?;
+    log.info(format!("Authenticating using ssh-agent at '{path}'"))?;
+    let ssh_agent_client = Client::connect(Path::new(path.as_str()))?;
+    match authenticate("/etc/sudo_ssh_keys", ssh_agent_client)? {
+        true => Ok(()),
+        false => Err(anyhow!("Agent did not know of any of the allowed keys")),
+    }
+}
+
+// Just a quick hack to get logging into syslog. Longer term,
+// this should be done pam-bindings: https://github.com/anowell/pam-rs/pull/12
+
+const PREFIX: &str = "pam_ssh_agent(sudo:auth): ";
+
+struct SyslogLogger {
+    log: Logger<LoggerBackend, Formatter3164>,
+}
+
+impl SyslogLogger {
+    fn new() -> Self {
+        match syslog::unix(Formatter3164::default()) {
+            Ok(log) => SyslogLogger { log },
+            Err(e) => panic!("Failed to create syslog: {e:?}"),
         }
     }
 
-    fn sm_setcred(_pamh: &mut PamHandle, _args: Vec<&CStr>, _flags: PamFlag) -> PamResultCode {
-        PamResultCode::PAM_SUCCESS
+    fn info<S: Display>(&mut self, message: S) -> Result<()> {
+        self.log
+            .info(format!("{PREFIX}{message}"))
+            .map_err(|e| anyhow!("failed to log: {e:?}"))
+    }
+
+    fn error<S: Display>(&mut self, message: S) -> Result<()> {
+        self.log
+            .err(format!("{PREFIX}{message}"))
+            .map_err(|e| anyhow!("failed to log: {e:?}"))
     }
 }
