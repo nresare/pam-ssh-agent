6fc8a5143945658fd3ad6227163aa34c13820172
Noa Resare <noa@resare.com>
2025-08-16 15:08:46 +0100

    WIP: include script that creates .dsc

---
+++ create-deb-dsc.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -ex
+VERSION=0.9.3
+RUST_VERSION=1.82
+PATH=/usr/lib/rust-${RUST_VERSION}/bin:/usr/bin
+
+rm -rf vendor
+cargo vendor-filterer --platform "*-unknown-linux-gnu"
+tar cfJ ../pam-ssh-agent_${VERSION}.orig-vendor.tar.xz vendor
+
+tar cfJ ../pam-ssh-agent_${VERSION}.orig.tar.xz src examples tests \
+ .github LICENSE* README* create-deb-dsc.sh Cargo*
+
+dpkg-source --build .
